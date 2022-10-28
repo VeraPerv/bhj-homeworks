2ac6f85ad97cd48923b0094ee029990bb7b7edc9
Vera Pervushina <vera.pervushina2608@gmail.com>
2022-10-28 12:16:47 +0700

    last commit homework 2

--- element-search/slider/task.js
+++ element-search/slider/task.js
@@ -7,28 +7,27 @@ const sliderSamples = Array.from(document.querySelectorAll('.slider__item'));
 let index = 0;
 
 arrowNext.addEventListener('click', function () {
-
     sliderSamples[index].classList.remove('slider__item_active');
-    index += 1;
-    sliderSamples[index].classList.add('slider__item_active');
+
     if (index + 1 > sliderSamples.length - 1) {
         index = 0;
         sliderSamples[index].classList.add('slider__item_active');
+        return;
     }
+    index += 1;
+    sliderSamples[index].classList.add('slider__item_active');
+
 });
 
 
-let indexPrev = 5;
+
 arrowPrev.addEventListener('click', function () {
-    //debugger;
     sliderSamples[index].classList.remove('slider__item_active');
-    indexPrev -= 1;
-    sliderSamples[indexPrev].classList.add('slider__item_active');
-
-
-    if (indexPrev == 0) {
-        indexPrev = 4;
+    if (index == 0) {
+        index = sliderSamples.length - 1;
         sliderSamples[index].classList.add('slider__item_active');
+        return;
     }
+    index -= 1;
+    sliderSamples[index].classList.add('slider__item_active');
 });
-
